b17d6f759cc3caaa86288f37422546e177874c42
Dong Jun <121388798+Chan-Dong-Jun@users.noreply.github.com>
2023-07-10 17:02:52 +0800

    -

--- mainproj.py
+++ mainproj.py
@@ -7,15 +7,15 @@ import matplotlib.pyplot as plt
 tod = datetime.now()
 word_cloud_map = {}
 
-# date must be after 2015 to be safe cuz bbc news had no chinese web before then
+# period must be after 2015
 if (date := input("Pls input date in format (yyyymmdd): ")):
     tod = datetime.strptime(date, "%Y%m%d")
 
 multiplier = 1 # interval between each days 
-for no in range(4): # the integer in range indicates the number of days up to tod to scrape
+for no in range(5): 
     parse_article("https://web.archive.org/web/"+ str((tod-timedelta(days=no*multiplier)).strftime('%Y%m%d'))+"/https://www.bbc.com/zhongwen/simp", word_cloud_map)
 
-# creating wordcloud
+# create wordcloud
 font_path ="SourceHanSerifK-Light.otf"
 wc = WordCloud(font_path=font_path, background_color="white", max_words=2000, max_font_size=200, width=1000, height=860,random_state=1)
 wc.generate_from_frequencies(word_cloud_map)
